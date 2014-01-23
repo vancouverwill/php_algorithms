c1de7a8c60ddf00ff38c9a4d33118e27c32b6c2c
Will Melbourne imac <will_melbourne@hotmail.com>
2014-01-22 18:42:46 -0800

    working on BinarySearchTreeSymbolTable.php

--- searchin/symbol_tables/BinarySearchTreeSymbolTable.php
+++ searchin/symbol_tables/BinarySearchTreeSymbolTable.php
@@ -5,7 +5,7 @@
  * and open the template in the editor.
  */
 class BinarySearchTreeSymbolTable {
-    private $root;
+    private $root; // Node
     
     public function isEmpty()
     {
@@ -16,10 +16,17 @@ class BinarySearchTreeSymbolTable {
             return false;
         }
     }
-    
-    private function size($x)
+
+
+    /**
+     * @param node $node
+     * @return mixed
+     */
+    private function size($node = null)
     {
-        
+        if ($node == null) { return $this->size($this->root); }
+
+        return $node->getNum();
     }
     
     public function contains($key)
@@ -32,9 +39,78 @@ class BinarySearchTreeSymbolTable {
         }
     }
     
-    public function get($nodeX, $key)
+    public function get($key, $node = null)
     {
-        if ($nodeX == null) return null;
+        if ($node == null) { $node = $this->root; }
+        // todo finish
+    }
+
+    public function put($key, $val) {
+        if ($value == null) { $this->delete($key); return; }
+        $this->root = $this->put($this->root, $key, $val);
+        assert $this->check();
+    }
+
+    private function put_recursive($key, $value, $node)
+    {
+
+        if ($node == null) { $node = $this->root; }
+        $root
+
+    }
+
+
+
+    // check integrity of Binary Search Tree Symbol Table
+
+    private function check()
+    {
+        if (!$this->isBinarySearchTreeSymbolTable($this->root, null, null)) {
+            print "Not in symetric order";
+            return false;
+        }
+    }
+
+    // does this binary tree satisfy symmetric order?
+    // Note: this test also ensures that data structure is a binary tree since order is strict
+    // is the tree rooted at x a BinarySearchTreeSymbolTable with all keys strictly between min and max
+    // (if min or max is null, treat as empty constraint)
+    // Credit: Bob Dondero's elegant solution
+    private function isBinarySearchTreeSymbolTableRecursive($node, $minKey, $maxKey)
+    {
+        if ($node == null) return true;
+        if ($minKey != null && $node->getKey() <= $minKey) return false;
+        if ($maxKey != null && $node->getKey() >= $maxKey) return false;
+        return $this->isBinarySearchTreeSymbolTableRecursive($node->getLeft(), $minKey, $node->getKey()) && $this->isBinarySearchTreeSymbolTableRecursive($node->getRight(), $node->getKey(), $maxKey);
+    }
+
+
+    // are the size fields correct?
+    /*
+     * @param Node $node
+     * @return recursive
+     */
+    private function isSizeConsistent($node)
+    {
+        if ($node == null) return true;
+        if ($node->getNum() != $this->size($node->getLeft()) + $this->size($node->getRight()) + 1) return false;
+        return $this->isSizeConsistent($node->getLeft()) && $this->isSizeConsistent($node->getRight());
+    }
+
+
+
+    /**
+     *
+     * check that ranks are consistent
+     */
+    private function isRankConsistent() {
+        for ($i = 0; $i < $this->size(); $i++) {
+            if ($i != $this->rank($this->select($i))) return false;
+        }
+        foreach ($this->keys() AS $key) {
+            if ()ey.compareTo(select(rank(key))) != 0) return false;
+        }
+        return true;
     }
 }
 
@@ -51,7 +127,33 @@ class Node {
         $this->value = $value;
         $this->N = N;
     }
-    
-    
-    
+
+    public function getNum(){
+        return $this->N;
+    }
+
+    public function getKey(){
+        return $this->key;
+    }
+
+    public function getLeft(){
+        return $this->left;
+    }
+
+
+    public function getRight(){
+        return $this->right;
+    }
+
+    public function getValue(){
+        return $this->value;
+    }
+
+
 }
+
+//Example Usage
+
+$symbolTable = new BinarySearchTreeSymbolTable();
+
+$symbolTable->put("word", 1);
---
+++ todo.md
@@ -0,0 +1,1 @@
+working on sequential search symbol table
