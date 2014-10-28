f271d1855eada018a58ca4db01b4185610099d52
Will Melbourne <willmelbourne@gmail.com>
2014-10-27 22:35:36 -0700

    update spelling mistakes

--- sandbox/findTheFirstTwoNumbersWhichAddUpToAPair.php
+++ sandbox/findTheFirstTwoNumbersWhichAddUpToAPair.php
@@ -17,7 +17,10 @@ function findPair($sortedArray, $desiredNumber)
     $lo = 0;
     $hi = count($sortedArray) - 1;
 
-    for ($lo < count($desiredNumber - 1); $hi >= 0;) {
+//    for ($lo < count($desiredNumber - 1); $hi >= 0;) {
+    for ($lo < (count($sortedArray) - 1); $hi >= 0;) {
+
+        $temp =  count($desiredNumber - 1);
         if ($sortedArray[$lo] + $sortedArray[$hi] > $desiredNumber) {
             $hi--;
         }
@@ -32,4 +35,11 @@ function findPair($sortedArray, $desiredNumber)
     echo 'lo:' . $lo . ' .hi:' . $hi;
 }
 
-findPair($sortedArray, $desiredNumber);
+echo findPair($sortedArray, $desiredNumber);
+
+
+
+$sortedArray = array(0, 4, 6, 9, 13, 17, 24, 32);
+$desiredNumber = 49;
+
+echo findPair($sortedArray, $desiredNumber);
